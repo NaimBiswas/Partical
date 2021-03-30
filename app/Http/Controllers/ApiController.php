718ce49f7938e3ac209454163f439664fce16e3b
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-30 14:35:44 +0600

    Api registration Done

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+    }
+
+    public function skills()
+    {
+        $skill = Skill::all();
+        return response()->json($skill);
+    }
 }
